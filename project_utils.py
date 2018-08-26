cce7507cf39592b19e3d392f74a0bdaf8cbe9467
iyarzaks <iyarzaks@gmail.com>
2018-08-26 20:00:03 +0300

    add bag of words test

--- project_utils.py
+++ project_utils.py
@@ -1,10 +1,15 @@
 import pandas
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 def to_bag_of_words(articles):
-    vectorizer = TfidfVectorizer()
-    vectorizer.fit(articles)
-    print(vectorizer.vocabulary_)
-    print(vectorizer.idf_)
-    vector = vectorizer.transform([articles[0]])
-    print(vector.shape)
-    print(vector.toarray())
+    # vectorizer = TfidfVectorizer()
+    # vectorizer.fit(articles)
+    # print(vectorizer.vocabulary_)
+    # print(vectorizer.idf_)
+    # vector = vectorizer.transform([articles[0]])
+    # print(vector.shape)
+    # print(vector.toarray())
+    count_vect = CountVectorizer()
+    X_train_counts = count_vect.fit_transform(articles)
+    print (X_train_counts.shape)
+    print (X_train_counts[:,count_vect.vocabulary_['and']])
+    print (count_vect.vocabulary_)
