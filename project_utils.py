a2c50433639d191918d175ffa6becfd2f28bdcb2
iyarzaks <iyarzaks@gmail.com>
2018-08-26 23:56:30 +0300

    add json of important words.

--- project_utils.py
+++ project_utils.py
@@ -1,6 +1,8 @@
 import pandas
+import json
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
-def to_bag_of_words(articles):
+from sklearn.feature_selection import mutual_info_classif
+def to_bag_of_words(articles,labels):
     # vectorizer = TfidfVectorizer()
     # vectorizer.fit(articles)
     # print(vectorizer.vocabulary_)
@@ -8,8 +10,24 @@ def to_bag_of_words(articles):
     # vector = vectorizer.transform([articles[0]])
     # print(vector.shape)
     # print(vector.toarray())
-    count_vect = CountVectorizer()
+    categories = [0, 1]
+
+    count_vect = CountVectorizer(max_df=0.95, min_df=2,
+                                     max_features=50000,
+                                     stop_words='english')
     X_train_counts = count_vect.fit_transform(articles)
+    res = dict(zip(count_vect.get_feature_names(),
+               mutual_info_classif(X_train_counts, labels, discrete_features=True)
+               ))
+    #print (res)
     print (X_train_counts.shape)
-    print (X_train_counts[:,count_vect.vocabulary_['and']])
-    print (count_vect.vocabulary_)
+    print (X_train_counts[:,count_vect.vocabulary_['www']])
+    sorted_d = sorted(res.items(), key=lambda x: x[1])
+    print_to_json(sorted_d, "words_importance.json")
+    #print (count_vect.vocabulary_)
+    #print(count_vect.vocabulary_.get('algorithm'))
+
+
+def print_to_json(data, path):
+    with open(path, 'w') as outfile:
+        json.dump(data, outfile)
--- words_test.py
+++ words_test.py
@@ -3,4 +3,5 @@ from project_utils import *
 
 data = pd.read_csv('Classified_Data_kaggle.csv', engine='python')
 artcles_list = data['Body'].values.astype('U').tolist()
-to_bag_of_words(artcles_list)
+labels = data['Label (1=true)'].values.astype('U').tolist()
+to_bag_of_words(artcles_list,labels)
