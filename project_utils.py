63a94352323a36e9d4e1facfa189ff7544e297ba
iyarzaks <iyarzaks@gmail.com>
2018-09-01 13:05:18 +0300

    add logistic function.

--- project_utils.py
+++ project_utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 from sklearn.feature_selection import mutual_info_classif
+import numpy as np
 
 def to_bag_of_words(articles,labels):
     # vectorizer = TfidfVectorizer()
@@ -23,12 +24,36 @@ def to_bag_of_words(articles,labels):
     #print (res)
     print (X_train_counts.shape)
     print (X_train_counts[:,count_vect.vocabulary_['www']])
-    sorted_d = sorted(res.items(), key=lambda x: x[1])
+    sorted_d = sorted(res.items(), key=lambda x: -x[1])
+    top_50_words = [k for (k,v) in sorted_d[:50]]
+    print (top_50_words)
+    print_to_json(top_50_words,"top_50.json")
     print_to_json(sorted_d, "words_importance.json")
     #print (count_vect.vocabulary_)
     #print(count_vect.vocabulary_.get('algorithm'))
 
 
+def build_table_form_words(data,words):
+    articles_list = data['Body'].values.astype('U').tolist()
+    count_vect = CountVectorizer(max_df=0.95, min_df=2,
+                                 max_features=50000,
+                                 stop_words='english')
+    dtm = count_vect.fit_transform(articles_list)
+    df = pd.DataFrame(dtm.toarray(), columns=count_vect.get_feature_names())
+    df = pd.DataFrame (df ,columns=words)
+    result = pd.concat([df, data], axis =1)
+    return result
+
+
+    #filterd = df.loc[df[df.index.name].get_feature_name().isin(words)]
+    #print (filterd.shape)
+
 def print_to_json(data, path):
     with open(path, 'w') as outfile:
         json.dump(data, outfile)
+
+
+def read_json(name):
+    with open(name) as inputfile:
+        data = json.load(inputfile)
+        return data
--- words_test.py
+++ words_test.py
@@ -1,7 +1,19 @@
 import pandas as pd
 from project_utils import *
+from sklearn import linear_model, datasets
+
 
 data = pd.read_csv('Classified_Data_kaggle.csv', engine='python')
 artcles_list = data['Body'].values.astype('U').tolist()
 labels = data['Label (1=true)'].values.astype('U').tolist()
-to_bag_of_words(artcles_list,labels)
+words = read_json("top_50.json")
+#to_bag_of_words(artcles_list,labels)
+
+df_with_imp_words = build_table_form_words(data,words)
+X = df_with_imp_words.iloc[:100,:50]
+print (X)
+Y = df_with_imp_words['Label (1=true)']
+Y = Y.iloc[:100]
+logreg = linear_model.LogisticRegression(C=1e5)
+logreg.fit(X, Y)
+print (logreg.coef_)
